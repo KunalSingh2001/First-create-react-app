60e159727ae4ee263f56e27a5a808edb5a004e52
kunal <kunalsingh787711@gmail.com>
2025-07-17 07:45:50 +0530

    Create first custom components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import ExpansesList from "./components/ExpensesItem";
 function App() {
   return (
     <div>
         <h1>Hello World</h1>
         <p>dcdhcbhbdchb</p>
+        <ExpansesList></ExpansesList>
     </div>
   );
 }
---
+++ src/components/ExpensesItem.jsx
@@ -0,0 +1,4 @@
+function ExpansesList() {
+    return <p>This is list of components</p>
+}
+export default ExpansesList;
