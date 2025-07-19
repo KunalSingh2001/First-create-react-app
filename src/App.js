ee3571835296dd6490759e8bbf41b100af71c91b
kunal <kunalsingh787711@gmail.com>
2025-07-19 17:11:51 +0530

    Use of map to looping through the data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import ExpansesList from "./components/ExpensesItem";
 function App() {
+  const items = [
+    {id:1, date: new Date(2025, 7, 19), location:"Bangalore", description: "this is props", title: "Insurance", price:"$10"},
+    {id:2, date: new Date(2025, 7, 19), location:"Delhi", description: "this is props", title: "Book", price:"$20"},
+    {id:3, date: new Date(2025, 7, 19), location:"Hyderabad", description: "this is props", title: "Pen", price:"$30"},
+    {id:4, date: new Date(2025, 7, 19), location:"Mumbai", description: "this is props", title: "Laptop", price:"$40"}
+  ];
   return (
     <div>
         <h1>Hello World</h1>
         <p>dcdhcbhbdchb</p>
-        <ExpansesList date={new Date(2025, 7, 19)} location="Bangalore" description="this is props" title="Insurance" price="$10"></ExpansesList>
-        <ExpansesList date={new Date(2025, 7, 19)} location="Delhi" description="this is props" title="Book" price="$20"></ExpansesList>
-        <ExpansesList date={new Date(2025, 7, 19)} location="Hyderabad" description="this is props" title="Pen" price="$30"></ExpansesList>
-        <ExpansesList date={new Date(2025, 7, 19)} location="Mumbai" description="this is props" title="Laptop" price="$40"></ExpansesList>
+        {items.map((val, index) => {
+          return (
+            <ExpansesList date={val.date} location={val.location} description={val.description} title={val.title} price={val.price}></ExpansesList>
+          );
+        })}
     </div>
   );
 }
