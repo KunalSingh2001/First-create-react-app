a6f3939658a8c6a62165979b09b662bff0b6d648
kunal <kunalsingh787711@gmail.com>
2025-07-25 08:29:09 +0700

    Applied the validation into form using Formik form

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -1,23 +1,32 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
+import { Form, Formik, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup';
 const ExpansesForm = () => {
     //Both method are will work this is for small form
 
-    const [enteredTitle, setEnteredTitle] = useState('');
-    const [enteredAmount, setEnteredAmount] = useState('');
-    const [enteredDate, setEnteredDate] = useState('');
+    // const [enteredTitle, setEnteredTitle] = useState('');    //We don't need of store state while we using Formik because its saving sate by its own
+    // const [enteredAmount, setEnteredAmount] = useState('');
+    // const [enteredDate, setEnteredDate] = useState('');
 
-    const titleChangeHandler = (event) => {
-        setEnteredTitle(event.target.value)
-    }
 
-    const amountChangeHandler = (event) => {
-        setEnteredAmount(event.target.value)
-    }
+    const validationSchema = Yup.object({
+        title: Yup.string().required("Title is required"),
+        amount: Yup.number().required("amount is required"),
+        date: Yup.string().required('date is required')
+    })
 
-    const dateChangeHandler = (event) => {
-        setEnteredDate(event.target.value)
-    }
+    // const titleChangeHandler = (event) => {
+    //     setEnteredTitle(event.target.value)
+    // }
+
+    // const amountChangeHandler = (event) => {    //We don't need of store state while we using Formik because its saving sate by its own
+    //     setEnteredAmount(event.target.value)
+    // }
+
+    // const dateChangeHandler = (event) => {
+    //     setEnteredDate(event.target.value)
+    // }
 
 
     //We can use this approch for large form al
@@ -76,39 +85,51 @@ const ExpansesForm = () => {
     //         }
     //     });
     // }
-
-    const formSubmitHandler = (event) => {
-        event.preventDefault();
-        const obj = {
-            title :enteredTitle,
-            amount: enteredAmount,
-            date: enteredDate
-        }
-        console.log(obj);
-        setEnteredTitle("");
-        setEnteredAmount("");
-        setEnteredDate("");
+    const obj = {
+        title: "",
+        amount: "",
+        date: ""
     }
+    // const formSubmitHandler = (event) => {
+    //     event.preventDefault();
 
-    return <form onSubmit={formSubmitHandler}>
-        <div className="new-expense__controls">
-            <div className="new-expense__control">
-                <label htmlFor="title">Title</label>
-                <input type="text" id="title" onChange={titleChangeHandler} value={enteredTitle} />
+    //     // setEnteredTitle("");
+    //     // setEnteredAmount("");   //We don't need of store state while we using Formik because its saving sate by its own
+    //     // setEnteredDate("");
+    //     resetForm();
+    // }
+
+    const formSubmitHandler = (values, { resetForm }) => {
+        console.log('Form data:', values);
+        resetForm();
+    };
+
+
+    //Note :- we don't need to define value and onChnage in each field while we are using Formik ok
+    return <Formik initialValues={obj} validationSchema={validationSchema} onSubmit={formSubmitHandler}>
+        <Form>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label htmlFor="title">Title</label>
+                    <Field type="text" name="title" id="title"/>
+                    <ErrorMessage name="title" component="div" className="text-red-500 text-md"/>
+                </div>
+                <div className="new-expense__control">
+                    <label htmlFor="amount">Amount</label>
+                    <Field type="number" name="amount" id="amount"/>
+                    <ErrorMessage name="amount" component="div" className="text-red-500 text-md"/>
+                </div>
+                <div className="new-expense__control">
+                    <label htmlFor="date">Date</label>
+                    <Field name ="date" type="date" id="date" min="2023-01-01" max="2024-12-31"/>
+                    <ErrorMessage name="date" component="div" className="text-red-500 text-md" />
+                </div>
             </div>
-            <div className="new-expense__control">
-                <label htmlFor="amount">Amount</label>
-                <input type="number" id="amount" onChange={amountChangeHandler} value={enteredAmount} />
+            <div className="new-expense__actions">
+                <button type="submit">Add Expense</button>
             </div>
-            <div className="new-expense__control">
-                <label htmlFor="date">Date</label>
-                <input type="date" id="date" min="2023-01-01" max="2024-12-31" onChange={dateChangeHandler} value={enteredDate} />
-            </div>
-        </div>
-        <div className="new-expense__actions">
-            <button type="submit">Add Expense</button>
-        </div>
-    </form>
+        </Form>
+    </Formik>;
 }
 
 export default ExpansesForm;
